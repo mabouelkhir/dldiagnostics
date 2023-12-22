a7d9099e7f8b0e373dc577be1e42a50e475bbf70
mabouelkhir <mohamedabouelkhir34@gmail.com>
2023-12-22 21:06:01 +0100

    commit

--- lib/pages/secretaire/secretaireHomePage.dart
+++ lib/pages/secretaire/secretaireHomePage.dart
@@ -14,42 +14,9 @@ class SecretaireHomePageState extends State<SecretaireHomePage> {
   Image? image;
   bool? _loadingInProgress;
   ApiService apiService= ApiService();
-  static var userData;
-  static String? ImageOfUser;
-  static var idDoctor;
-
   @override
   void initState() {
     super.initState();
-
-    super.initState();
-    WidgetsBinding.instance!.addPostFrameCallback((_) async {
-      getActualUser().then((userData) {
-        var doctor = SecretaireM.fromJson(userData['medecin']);
-        setState(() {
-          DrawerDoctorScreenState.idDoctor = doctor.id;
-        });
-        DrawerDoctorScreenState.idDoctor = doctor.id;
-        //storeUser();
-
-        setState(() {
-          LoginPageState.username;
-          LoginPageState.firstName=doctor.nom;
-          LoginPageState.lastName=doctor.prenom;
-
-          DoctorHomePageState.idDoctor=doctor.id;
-          print("ttttttt");
-          print(doctor.id);
-          print(DoctorHomePageState.idDoctor);
-        });
-      });
-    });
-  }
-  Future getActualUser() async {
-    var res = await apiService.getMyProfile();
-    userData = json.decode(res.body);
-
-    return userData;
   }
 
   void displayImage() async {
@@ -62,47 +29,47 @@ class SecretaireHomePageState extends State<SecretaireHomePage> {
       body: NestedScrollView(
         headerSliverBuilder: ((context, innerBoxIsScrolled) => [
           SliverAppBar(
-                  actions: [
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 7, 7, 0),
-                      child: FloatingActionButton.small(
-                        onPressed: () {},
-                        child: FutureBuilder(
-                          future: apiService.getSecretaireOfUser(),
-                          builder:
-                              (BuildContext context, AsyncSnapshot snapshot) {
-                            if (snapshot.connectionState !=
-                                ConnectionState.done) {
-                              return CircularProgressIndicator();// your widget while loading
-                            }
+              actions: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 7, 7, 0),
+                  child: FloatingActionButton.small(
+                    onPressed: () {},
+                    child: FutureBuilder(
+                      future: apiService.getSecretaireOfUser(),
+                      builder:
+                          (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.connectionState !=
+                            ConnectionState.done) {
+                          return CircularProgressIndicator();// your widget while loading
+                        }
 
-                            if (!snapshot.hasData) {
-                              return Container(); //your widget when error happens
-                            }
+                        if (!snapshot.hasData) {
+                          return Container(); //your widget when error happens
+                        }
 
-                            var res = snapshot.data; //your Map<String,dynamic>
-                            var data=json.decode(res.body); 
+                        var res = snapshot.data; //your Map<String,dynamic>
+                        var data=json.decode(res.body);
 
-                            return ClipOval(
-                                child: Image.memory(
+                        return ClipOval(
+                            child: Image.memory(
                               base64Decode(
                                   data['photo'].toString()),
                             )
-                            ); //place your widget here
-                          },
-                        ),
-                        /*  ClipOval(
+                        ); //place your widget here
+                      },
+                    ),
+                    /*  ClipOval(
                       child: Image.memory(
                     base64Decode(DrawerPatientScreenState.ImageOfUser!),
                   )
                   ), */
-                      ),
-                    ),
-                  ],
-                  backgroundColor: Color(0xFF0857de),
-                  elevation: 0,
-                  leading: MenuSecretaireWidget())
-              /* SliverAppBar(
+                  ),
+                ),
+              ],
+              backgroundColor: Color(0xFF0857de),
+              elevation: 0,
+              leading: MenuSecretaireWidget())
+          /* SliverAppBar(
                   actions: [
                     Container(
                       margin: EdgeInsets.fromLTRB(0, 7, 7, 0),
@@ -118,7 +85,7 @@ class SecretaireHomePageState extends State<SecretaireHomePage> {
                   backgroundColor: Color(0xFF0857de),
                   elevation: 0,
                   leading: MenuSecretaireWidget()) */
-            ]),
+        ]),
         body: BodySecretaire(),
       ),
       //body: Body(),
